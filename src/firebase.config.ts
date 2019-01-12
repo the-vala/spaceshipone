9fd74f8fd8c62aa6edf0b9ecb832bcc8790db367
Diego Valadez <the-vala@users.noreply.github.com>
2019-01-11 19:44:46 -0600

    Firebase configuration

---
+++ src/firebase.config.ts
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyAF5zkGfQutCFf5xWg2A4TLtReWP4bRAuA",
+authDomain: "spaceshipone-dd5e2.firebaseapp.com",
+databaseURL: "https://spaceshipone-dd5e2.firebaseio.com",
+projectId: "spaceshipone-dd5e2",
+storageBucket: "spaceshipone-dd5e2.appspot.com",
+messagingSenderId: "162688486294"
+};
+firebase.initializeApp(config);
